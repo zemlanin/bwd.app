4c4b248b82f34390136cc8b26998d68a1c709cab
Anton Verinov <git@zemlan.in>
2015-04-11 12:09:49 +0300

    Initial Commit

---
+++ blackwidow/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  blackwidow
+//
+//  Created by Anton Verinov on 11.04.15.
+//  Copyright (c) 2015 ifelse. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
